d44de9cd8af64efd8112e70f7c1e755a5d386af7
Samuel K. Grush <Samuel@Grush.org>
2017-11-30 12:26:18 -0600

    Added Rel-to-SVG functions to relationalText

--- src/parser/relationalText.tsx
+++ src/parser/relationalText.tsx
@@ -382,3 +382,298 @@ export function htmlHLR(hlr: Rel.HighLevelRelationish) {
   console.error("unknown HLR:", hlr)
   throw new Error("Unknown type passed to htmlHLR")
 }
+
+
+export function svgRelAggregation(agg: Rel.Aggregation) {
+  let attrs: JSX.Element | null = null
+  if (agg.attributes && agg.attributes.length)
+    attrs = (
+      <tspan baselineShift="sub" className="columns">
+        (
+          {svgColumnList(agg.attributes)}
+        )
+      </tspan>
+    )
+
+  const aggregJsx = (
+    <tspan className="RelAggregation">
+      {attrs}
+      <tspan className="operator">{getSymbol('aggregation')}</tspan>
+      <tspan baselineShift="sub" className="functions">
+        {svgColumnList(agg.functions)}
+      </tspan>
+    </tspan>
+  )
+
+  if (agg.renames.length)
+    return svgRelRenameAggregate(agg.renames, aggregJsx)
+  else
+    return aggregJsx
+}
+
+
+export function svgRelRestriction(res: Rel.Restriction) {
+  const SYM = getSymbol('restriction')
+  const COND = svgRelConditional(res.conditions)
+  return (
+    <tspan className="RelRestriction">
+      <tspan className="operator">{SYM}</tspan>
+      <tspan baselineShift="sub" className="condition">
+        {COND}
+      </tspan>
+    </tspan>
+  )
+}
+
+export function svgRelProjection(res: Rel.Projection) {
+  const SYM = getSymbol('projection')
+  return (
+    <tspan className="RelProjection">
+      <tspan className="operator">{SYM}</tspan>
+      <tspan baselineShift="sub" className="columns">
+        {svgColumnList(res.columns)}
+      </tspan>
+    </tspan>
+  )
+}
+
+export function svgColumnList(cols: Array<Rel.Columnish>
+  ): Array<string|JSX.Element> {
+  const columns: Array<string|JSX.Element> = []
+  cols.forEach((col, idx) => {
+    if (idx > 0)
+      columns.push(",")
+    if (col instanceof Rel.Column)
+      columns.push(svgRelColumn(col, idx))
+    else if (col instanceof Rel.RelFunction)
+      columns.push(svgRelFunction(col, idx))
+    else
+      columns.push(col)
+  })
+  return columns
+}
+
+export function svgRelColumn(col: Rel.Column, iter?: number) {
+
+  if (col.as) {
+    return (
+      <tspan className="RelColumn" key={iter}>
+        <tspan className="column-as">{col.as}</tspan>
+      </tspan>
+    )
+  }
+
+  if (!col.relation) {
+    return (
+      <tspan className="RelColumn" key={iter}>
+        <tspan className="column-name">{svgGetName(col.target)}</tspan>
+      </tspan>
+    )
+  }
+
+  return (
+    <tspan className="RelColumn" key={iter}>
+      <tspan className="relation-name">{svgGetName(col.relation)}</tspan>
+      .
+      <tspan className="column-name">{svgGetName(col.target)}</tspan>
+    </tspan>
+  )
+}
+
+export function svgRelFunction(funct: Rel.RelFunction, idx?) {
+  const NAME = funct.fname.toUpperCase()
+  const EXPR = funct.expr === '*'
+          ? '*'
+          : svgRelColumn(funct.expr)
+
+  return (
+    <tspan className="RelFunction" key={idx}>
+      <tspan className="function-name">{NAME}</tspan>
+      (
+        {EXPR}
+      )
+    </tspan>
+  )
+}
+
+export function svgGetName(thing) {
+  if (typeof(thing) === 'string')
+    return thing
+  if (thing instanceof Rel.Relation)
+    return thing.name
+  if (thing instanceof Rel.Column)
+    return thing.as || svgRelColumn(thing)
+  if (thing instanceof Rel.RelFunction)
+    return svgRelFunction(thing)
+  if (thing instanceof Catalog.Column)
+    return thing.name
+  console.info("svgGetName", thing)
+  throw new Error("unexpected thing to svgGetName")
+}
+
+export function svgRelRenameAggregate(renames: string[], aggregJsx: JSX.Element) {
+  const SYM = getSymbol('rename')
+  const OUTPUT = svgColumnList(renames)
+  return (
+    <tspan className="RelRename RelRename-aggregation">
+      <tspan className="operator">{SYM}</tspan>
+      <tspan baselineShift="sub" className="condition">
+        {OUTPUT}
+      </tspan>
+      (
+        {aggregJsx}
+      )
+    </tspan>
+  )
+}
+
+export function svgRelRename(ren: Rel.Rename) {
+  const SYM = getSymbol('rename')
+  const INPUT = svgGetName(ren.input)
+  const OUTPUT = ren.output
+
+  // R as S  =>  ρ_S (R)
+  if (ren.input === ren.args && ren.input instanceof Rel.Relation)
+    return (
+      <tspan className="RelRename RelRename-unary">
+        <tspan className="operator">{SYM}</tspan>
+        <tspan baselineShift="sub" className="condition">
+          {OUTPUT}
+        </tspan>
+      </tspan>
+    )
+
+  // R.a as b  =>  ρ_{b∕R.a}(R)
+  // R as S  =>  ρ_{S∕R}(...)
+  return (
+    <tspan className="RelRename">
+      <tspan className="operator">{SYM}</tspan>
+      <tspan baselineShift="sub" className="condition">
+        {OUTPUT} {getSymbol('rename-divider')} {INPUT}
+      </tspan>
+    </tspan>
+  )
+}
+
+export function svgRelRelation(rel: Rel.Relation) {
+  const NAME = rel.name
+  return (
+    <tspan className="RelRelation">
+      {NAME}
+    </tspan>
+  )
+}
+
+export function svgRelJoinHelper(join: Rel.Join): [string, JSX.Element | null] {
+  if (typeof(join.condition) === 'string') {
+    return [getSymbol(join.condition), null]
+  } else if (join.condition instanceof Rel.Conditional) {
+    let cond = svgRelConditional(join.condition)
+    if (cond) {
+      cond = (
+        <tspan baselineShift="sub" className="condition">
+          {cond}
+        </tspan>
+      )
+    }
+    return [getSymbol('join'), cond]
+  } else {
+    throw new Error(`unknown RelJoin condition ${join.condition}`)
+  }
+}
+
+export function svgRelJoin(join: Rel.Join) {
+  const [joinSymbol, cond] = svgRelJoinHelper(join)
+  const LHS = svgHLR(join.lhs)
+  const RHS = svgHLR(join.rhs)
+
+  return (
+    <tspan className="RelJoin">
+      {LHS}
+      <tspan className="operator">{joinSymbol}</tspan>
+      {cond}
+      {RHS}
+    </tspan>
+  )
+}
+
+export function svgRelOperation(op: Rel.Operation) {
+  const OPSYM = getSymbol(op.op)
+  const LHS = svgRelOperand(op.lhs)
+  const RHS = svgRelOperand(op.rhs)
+
+  return (
+    <tspan className="RelOperation">
+      {LHS}
+      <tspan className="operator">{OPSYM}</tspan>
+      {RHS}
+    </tspan>
+  )
+}
+
+export function svgRelOperand(operand: Rel.OperandType|Rel.HighLevelRelationish) {
+  if (typeof(operand) === 'string')
+    return operand
+  if (operand instanceof Rel.RelFunction)
+    return svgRelFunction(operand)
+  if (operand instanceof Rel.Operation)
+    return svgRelOperation(operand)
+  if (operand instanceof Rel.Column)
+    return svgRelColumn(operand)
+  // throw new Error("Unexpected operand type")
+  return svgHLR(operand)
+}
+
+export function svgRelConditional(cond: Rel.Conditional) {
+  const OPSYM = getSymbol(cond.operation)
+
+  let lhs
+  let rhs
+
+  if (cond.lhs instanceof Rel.Conditional)
+    lhs = svgRelConditional(cond.lhs)
+  else if (cond.lhs instanceof Rel.RelFunction)
+    lhs = svgRelFunction(cond.lhs)
+  else
+    lhs = svgRelOperand(cond.lhs)
+
+  if (cond.rhs instanceof Rel.Conditional)
+    rhs = svgRelConditional(cond.rhs)
+  else if (Array.isArray(cond.rhs))
+    rhs = cond.rhs.map(svgRelOperand)
+  else if (cond.rhs instanceof Rel.RelFunction)
+    rhs = svgRelFunction(cond.rhs)
+  else
+    rhs = svgRelOperand(cond.rhs)
+
+  return (
+    <tspan className="RelConditional">
+      <tspan className="lhs">
+        {lhs}
+      </tspan>
+      <tspan className="operator">{OPSYM}</tspan>
+      <tspan className="rhs">
+        {rhs}
+      </tspan>
+    </tspan>
+  )
+}
+
+export function svgHLR(hlr: Rel.HighLevelRelationish) {
+  if (hlr instanceof Rel.Restriction)
+    return svgRelRestriction(hlr)
+  if (hlr instanceof Rel.Projection)
+    return svgRelProjection(hlr)
+  if (hlr instanceof Rel.Rename)
+    return svgRelRename(hlr)
+  if (hlr instanceof Rel.Operation)
+    return svgRelOperation(hlr)
+  if (hlr instanceof Rel.Relation)
+    return svgRelRelation(hlr)
+  if (hlr instanceof Rel.Join)
+    return svgRelJoin(hlr)
+  if (hlr instanceof Rel.Aggregation)
+    return svgRelAggregation(hlr)
+  console.error("unknown HLR:", hlr)
+  throw new Error("Unknown type passed to svgHLR")
+}
